29715c800ceae9bfdc6e01e6acae4ed3ab9ea4cc
Sonia <soniawadji@gmail.com>
2023-05-06 18:18:14 +0530

    Added a dialog box on click of add a playlist. Opening a dialog box. Code is too cluttered

--- app/src/main/java/com/example/moviesapp/ui/PlaylistBottomSheet.kt
+++ app/src/main/java/com/example/moviesapp/ui/PlaylistBottomSheet.kt
@@ -1,9 +1,13 @@
 package com.example.moviesapp.ui
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import com.example.moviesapp.R
+import com.example.moviesapp.databinding.DialogAddAPlaylistBinding
 import com.example.moviesapp.databinding.PlaylistBottomSheetBinding
 import com.example.moviesapp.model.Playlist
 import com.example.moviesapp.ui.adapter.PlaylistAdapter
@@ -13,6 +17,7 @@ class PlaylistBottomSheet : BottomSheetDialogFragment() {
 
     private lateinit var binding: PlaylistBottomSheetBinding
     private var playlistAdapter = PlaylistAdapter()
+    private var alertDialog: AlertDialog? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -22,6 +27,8 @@ class PlaylistBottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        createAddAPlaylistDialog()
 
         playlistAdapter.apply {
             playlists = arrayListOf(
@@ -33,13 +40,40 @@ class PlaylistBottomSheet : BottomSheetDialogFragment() {
             )
             onClick = { position ->
                 playlists?.let {
-                    if (position == it.size - 1) println("Add a playlist")
+                    if (position == it.size - 1) {
+                        alertDialog?.show()
+                    }
                 }
             }
         }
 
 
         binding.playlistRecyclerView.adapter = playlistAdapter
+    }
+
+    private fun createAddAPlaylistDialog() {
+        alertDialog = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater;
+            val binding = DialogAddAPlaylistBinding.inflate(inflater)
+            val editText = binding.playlistEdittext
+
+            builder.setView(binding.root).setPositiveButton(
+                R.string.ok
+            ) { dialog, _ ->
+                val playlistName = editText.text.toString()
+                if (playlistName.isNotEmpty()) {
+                    playlistAdapter.playlists?.add(Playlist(6, playlistName))
+                    playlistAdapter.notifyDataSetChanged()
+                    dialog.dismiss()
+                }
+            }.setNegativeButton(
+                R.string.cancel
+            ) { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.create()
+        }
     }
 
     companion object {
