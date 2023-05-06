20adc42c446208d78602e2f26d87b33b5964f78a
Sonia <soniawadji@gmail.com>
2023-05-06 17:41:00 +0530

    Added logic to add a playlist click.

--- app/src/main/java/com/example/moviesapp/ui/PlaylistBottomSheet.kt
+++ app/src/main/java/com/example/moviesapp/ui/PlaylistBottomSheet.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import com.example.moviesapp.databinding.PlaylistBottomSheetBinding
 import com.example.moviesapp.model.Playlist
 import com.example.moviesapp.ui.adapter.PlaylistAdapter
@@ -24,16 +23,21 @@ class PlaylistBottomSheet : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val options = listOf(
-            "Playlist 1", "Playlist 2", "Playlist 3", "Playlist 4"
-        )
+        playlistAdapter.apply {
+            playlists = arrayListOf(
+                Playlist(1, "Playlist 1"),
+                Playlist(2, "Playlist 2"),
+                Playlist(3, "Playlist 3"),
+                Playlist(4, "Playlist 4"),
+                Playlist(5, "+ Add a playlist")
+            )
+            onClick = { position ->
+                playlists?.let {
+                    if (position == it.size - 1) println("Add a playlist")
+                }
+            }
+        }
 
-        playlistAdapter.playlists = arrayListOf(
-            Playlist(1, "Playlist 1"),
-            Playlist(2, "Playlist 2"),
-            Playlist(3, "Playlist 3"),
-            Playlist(4, "Playlist 4"),
-        )
 
         binding.playlistRecyclerView.adapter = playlistAdapter
     }
--- app/src/main/java/com/example/moviesapp/ui/adapter/PlaylistAdapter.kt
+++ app/src/main/java/com/example/moviesapp/ui/adapter/PlaylistAdapter.kt
@@ -9,6 +9,7 @@ import com.example.moviesapp.model.Playlist
 class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder>() {
 
     var playlists: ArrayList<Playlist>? = null
+    var onClick: ((Int) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaylistViewHolder {
         val binding =
@@ -25,11 +26,14 @@ class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder>
 
     override fun getItemCount(): Int = playlists?.size ?: 0
 
-    class PlaylistViewHolder(binding: ItemPlaylistBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class PlaylistViewHolder(binding: ItemPlaylistBinding) : RecyclerView.ViewHolder(binding.root) {
         private val playlistTextView = binding.playlistTextView
 
         fun bind(playlist: Playlist, position: Int) {
             playlistTextView.text = playlist.playlistName ?: "Playlist ${position + 1}"
+            playlistTextView.setOnClickListener {
+                onClick?.invoke(position)
+            }
         }
     }
 }
