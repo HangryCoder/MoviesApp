d7534d4a3c1451739e1107b746ceb155cb5372a4
Sonia <soniawadji@gmail.com>
2023-06-18 16:30:28 +0530

    Removed unused code

--- app/src/main/java/com/example/moviesapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/moviesapp/di/ApplicationComponent.kt
@@ -1,27 +1,14 @@
 package com.example.moviesapp.di
 
-import android.app.Application
 import com.example.moviesapp.MyApplication
-import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
-import dagger.android.AndroidInjector
 import javax.inject.Singleton
 
 @Singleton
 @Component(
-    modules = [AndroidInjectionModule::class, ActivityModule::class,/* SubcomponentsModule::class,*/ ApplicationModule::class]
+    modules = [AndroidInjectionModule::class, ActivityModule::class, ApplicationModule::class]
 )
-interface ApplicationComponent  {
-    //fun activityComponent(): ActivityComponent.Factory
-
-    /*@Component.Builder
-    interface Builder {
-        fun build(): ApplicationComponent
-
-        @BindsInstance
-        fun application(application: Application): Builder
-    }*/
-
-     fun inject(application: MyApplication)
+interface ApplicationComponent {
+    fun inject(application: MyApplication)
 }
--- app/src/main/java/com/example/moviesapp/di/SubcomponentsModule.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moviesapp.di
-
-import dagger.Module
-
-@Module(subcomponents = [ActivityComponent::class])
-class SubcomponentsModule
