9f4645ff768afe8d2a15fe54d621ef6d8782b339
Sonia <soniawadji@gmail.com>
2023-06-18 10:46:21 +0530

    Viewmodel of activity instance is shared among fragments

--- app/src/main/java/com/example/moviesapp/di/ActivityModule.kt
+++ app/src/main/java/com/example/moviesapp/di/ActivityModule.kt
@@ -1,11 +1,23 @@
 package com.example.moviesapp.di
 
+import androidx.lifecycle.ViewModelProvider
+import com.example.moviesapp.ui.MainActivity
 import com.example.moviesapp.ui.adapter.MovieAdapter
+import com.example.moviesapp.viewmodel.MovieViewModel
 import dagger.Module
 import dagger.Provides
 
 @Module(includes = [MainActivityFragmentsModule::class])
 class ActivityModule {
+
+    @ActivityScope
+    @Provides
+    fun provideMoviesViewModel(
+        activity: MainActivity,
+        factory: AppViewModelFactory
+    ): MovieViewModel {
+        return ViewModelProvider(activity, factory)[MovieViewModel::class.java]
+    }
 
     @Provides
     fun provideAdapter(): MovieAdapter = MovieAdapter()
--- app/src/main/java/com/example/moviesapp/di/MainActivityModule.kt
+++ app/src/main/java/com/example/moviesapp/di/MainActivityModule.kt
@@ -1,12 +1,22 @@
 package com.example.moviesapp.di
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.moviesapp.ui.AddPlaylistDialog
 import com.example.moviesapp.ui.MainActivity
 import com.example.moviesapp.ui.PlaylistBottomSheet
+import com.example.moviesapp.viewmodel.MovieViewModel
+import dagger.Binds
+import dagger.MapKey
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+import kotlin.reflect.KClass
 
-@Module
+@Module(includes = [MovieViewModelModule::class])
 abstract class MainActivityModule {
     @ActivityScope
     @ContributesAndroidInjector(modules = [ActivityModule::class])
@@ -24,3 +34,47 @@ abstract class MainActivityFragmentsModule {
     @ContributesAndroidInjector
     abstract fun contributeAddPlaylistDialog(): AddPlaylistDialog
 }
+
+@Module
+abstract class MovieViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(MovieViewModel::class)
+    internal abstract fun bindsViewModel(viewModel: MovieViewModel): ViewModel
+}
+
+@Suppress("UNCHECKED_CAST")
+@Singleton
+class AppViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        var creator: Provider<out ViewModel>? = creators[modelClass]
+        if (creator == null) {
+            //Check if there is a subclass of ViewModel of type T
+            for ((key, value) in creators) {
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+        if (creator == null) {
+            throw IllegalArgumentException("unknown model class $modelClass")
+        }
+        try {
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+
+    }
+}
+
+@MustBeDocumented
+@Retention
+@Target(
+    AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER
+)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
